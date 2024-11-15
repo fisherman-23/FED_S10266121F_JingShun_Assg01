f996a19cee941c31071e8b44c4efeb08079fde8a
Jing Shun <ooijs234@gmail.com>
2024-11-16 02:06:45 +0800

    feat: add hacker text effect to h1

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ elements.forEach((element) => {
     console.log("Touch event started");
   });
 });
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+
+// Add the hackedTextEffect when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector("h1");
+
+  // Trigger the effect on initial load
+  hackedTextEffect({ target: h1 });
+
+  // Add event listener for mouseover after initial load
+  h1.onmouseover = (event) => hackedTextEffect(event);
+});
+
+function hackedTextEffect(event) {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
